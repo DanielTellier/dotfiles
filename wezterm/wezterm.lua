9573c322d00fdd33b406b6371d8a4df61d179d06
DanielTellier <telldanieljames@gmail.com>
2024-08-01 11:14:33 -0700

    Add key map for vim and wezterm.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -48,6 +48,7 @@ return {
         {key = 'o', mods="LEADER", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
         {key = 'v', mods="LEADER", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
         {key = 't', mods="LEADER", action=act{SpawnTab="CurrentPaneDomain"}},
+        {key = 'n', mods="SHIFT|CTRL", action=act.SpawnWindow},
         {key = 'n', mods="LEADER", action=act.ActivateTabRelative(1)},
         {key = 'p', mods="LEADER", action=act.ActivateTabRelative(-1)},
         {key = 'q', mods="LEADER", action=act.CloseCurrentTab{confirm = false }},
