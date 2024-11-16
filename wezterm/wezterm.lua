150c1f5cd38ec04047903a8927bf0ffe8d18c1e4
DanielTellier <telldanieljames@gmail.com>
2024-11-16 12:14:46 -0800

    Modify key mappings for linux vs mac.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,75 @@ local wezterm = require('wezterm')
 local act = wezterm.action
 local init = require('init')
 
+local function os_capture(cmd, raw)
+    local out = assert(io.popen(cmd, 'r'))
+    local out_str = assert(out:read('*a'))
+    out:close()
+    if raw then return out_str end
+    -- %s indicates a space
+    out_str = string.gsub(out_str, '^%s+', '')
+    out_str = string.gsub(out_str, '%s+$', '')
+    out_str = string.gsub(out_str, '[\n\r]+', '')
+    return out_str
+end
+
 wezterm.on('gui-startup', function(window, pane)
     init.startup({})
 end)
 -- wezterm.on('update-right-status', function(window, pane)
 --     window:set_right_status(window:active_workspace())
 -- end)
+
+keys = {
+    -- Send "CTRL-t" to the terminal when pressing CTRL-t, CTRL-t
+    {key = 't', mods="LEADER|CTRL", action=act.SendString('\x14')},
+    {key = 'o', mods="LEADER", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
+    {key = 'v', mods="LEADER", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    {key = 't', mods="LEADER", action=act{SpawnTab="CurrentPaneDomain"}},
+    {key = 'n', mods="SHIFT|CTRL", action=act.SpawnWindow},
+    {key = 'n', mods="LEADER", action=act.ActivateTabRelative(1)},
+    {key = 'p', mods="LEADER", action=act.ActivateTabRelative(-1)},
+    {key = 'q', mods="LEADER", action=act.CloseCurrentTab{confirm = false }},
+    {key = 'h', mods="LEADER", action=act{ActivatePaneDirection="Left"}},
+    {key = 'l', mods="LEADER", action=act{ActivatePaneDirection="Right"}},
+    {key = 'k', mods="LEADER", action=act{ActivatePaneDirection="Up"}},
+    {key = 'j', mods="LEADER", action=act{ActivatePaneDirection="Down"}},
+    {key = 'w', mods="LEADER", action=act.CloseCurrentPane{confirm = false}},
+    {key = 'z', mods="LEADER", action="TogglePaneZoomState"},
+    {key = 'f', mods="LEADER", action="ToggleFullScreen"},
+    {key = 'c', mods="LEADER", action="ActivateCopyMode"},
+    {key = 'UpArrow', mods="CTRL", action=act.ScrollByPage(-0.5)},
+    {key = 'DownArrow', mods="CTRL", action=act.ScrollByPage(0.5)},
+    {key = 'f', mods="CTRL|SHIFT", action=act.Search{CaseInSensitiveString=""}},
+    {key = 'r', mods="LEADER", action=act.RotatePanes("Clockwise")},
+    {key = '0', mods="LEADER", action=act.PaneSelect{mode="SwapWithActive"}},
+    {key = '1', mods="ALT", action=act.SwitchWorkspaceRelative(-1)},
+    {key = '2', mods="ALT", action=act.SwitchWorkspaceRelative(1)},
+    {key = '3', mods="ALT", action=act.ShowLauncherArgs{flags = 'FUZZY|WORKSPACES'}},
+    {key = 'Escape', mods="LEADER", action=act.QuitApplication},
+}
+
+if os_capture("uname", false) == "Linux" then
+    table.insert(
+        keys, {key = 'c', mods="CTRL|SHIFT", action=act.CopyTo("ClipboardAndPrimarySelection")}
+    )
+    table.insert(
+        keys, {key = 'v', mods="CTRL|SHIFT", action=act.PasteFrom("Clipboard")}
+    )
+    table.insert(
+        keys, {key = 'v', mods="CTRL|SHIFT", action=act.PasteFrom("PrimarySelection")}
+    )
+else
+    table.insert(
+        keys, {key = 'c', mods="CMD", action=act.CopyTo("ClipboardAndPrimarySelection")}
+    )
+    table.insert(keys,
+        {key = 'v', mods="CMD", action=act.PasteFrom("Clipboard")}
+    )
+    table.insert(
+        keys, {key = 'v', mods="CMD", action=act.PasteFrom("PrimarySelection")}
+    )
+end
 
 return {
     -- General
@@ -42,36 +105,6 @@ return {
     leader = { key="t", mods="CTRL", timeout_milliseconds=2000 },
 
     -- Key bindings
-    keys = {
-        -- Send "CTRL-t" to the terminal when pressing CTRL-t, CTRL-t
-        {key = 't', mods="LEADER|CTRL", action=act.SendString('\x14')},
-        {key = 'o', mods="LEADER", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
-        {key = 'v', mods="LEADER", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
-        {key = 't', mods="LEADER", action=act{SpawnTab="CurrentPaneDomain"}},
-        {key = 'n', mods="SHIFT|CTRL", action=act.SpawnWindow},
-        {key = 'n', mods="LEADER", action=act.ActivateTabRelative(1)},
-        {key = 'p', mods="LEADER", action=act.ActivateTabRelative(-1)},
-        {key = 'q', mods="LEADER", action=act.CloseCurrentTab{confirm = false }},
-        {key = 'h', mods="LEADER", action=act{ActivatePaneDirection="Left"}},
-        {key = 'l', mods="LEADER", action=act{ActivatePaneDirection="Right"}},
-        {key = 'k', mods="LEADER", action=act{ActivatePaneDirection="Up"}},
-        {key = 'j', mods="LEADER", action=act{ActivatePaneDirection="Down"}},
-        {key = 'w', mods="LEADER", action=act.CloseCurrentPane{confirm = false}},
-        {key = 'z', mods="LEADER", action="TogglePaneZoomState"},
-        {key = 'f', mods="LEADER", action="ToggleFullScreen"},
-        {key = 'c', mods="LEADER", action="ActivateCopyMode"},
-        {key = 'UpArrow', mods="CTRL", action=act.ScrollByPage(-0.5)},
-        {key = 'DownArrow', mods="CTRL", action=act.ScrollByPage(0.5)},
-        {key = 'c', mods="CMD", action=act.CopyTo("ClipboardAndPrimarySelection")},
-        {key = 'v', mods="CMD", action=act.PasteFrom("Clipboard")},
-        {key = 'v', mods="CMD", action=act.PasteFrom("PrimarySelection")},
-        {key = 'f', mods="CTRL|SHIFT", action=act.Search{CaseInSensitiveString=""}},
-        {key = 'r', mods="LEADER", action=act.RotatePanes("Clockwise")},
-        {key = '0', mods="LEADER", action=act.PaneSelect{mode="SwapWithActive"}},
-        {key = '1', mods="ALT", action=act.SwitchWorkspaceRelative(-1)},
-        {key = '2', mods="ALT", action=act.SwitchWorkspaceRelative(1)},
-        {key = '3', mods="ALT", action=act.ShowLauncherArgs{flags = 'FUZZY|WORKSPACES'}},
-        {key = 'Escape', mods="LEADER", action=act.QuitApplication},
-    },
+    keys = keys,
     unicode_version = 15,
 }
