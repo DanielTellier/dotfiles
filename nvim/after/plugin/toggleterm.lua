7485586ee0a19b44e2d57328ca97966718af1c81
DanielTellier <telldanieljames@gmail.com>
2025-03-21 16:54:01 -0700

    feat(toggleterm): add function to toggle terminal with index
    - Added `toggle_term_with_index` function to handle terminal toggling
      with a specified index.
    - Replaced `toggle_term_with_number` with `toggle_term_with_index` in
      key mappings.
    - Removed the old `toggle_term_with_number` function from utils.lua.

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,4 +1,22 @@
 local utils = require('utils')
+
+local function toggle_term_with_index(size, direction)
+    if tonumber(index) then
+        local cmd = ""
+        if direction == "tab" then
+            cmd = string.format(
+                "%sToggleTerm direction=%s", index, direction
+            )
+        else
+            cmd = string.format(
+                "%sToggleTerm size=%d direction=%s", index, size, direction
+            )
+        end
+        vim.cmd(cmd)
+    else
+        print("Invalid input! Please enter a number.")
+    end
+end
 utils.map('n', '<leader>tm', '<cmd>ToggleTerm<cr>', { desc = "Toggle terminal" })
 utils.map(
     'n',
@@ -8,16 +26,16 @@ utils.map(
 )
 utils.mapfunc(
     'n', '<leader>mv', function()
-        utils.toggle_term_with_number(90, "vertical")
+        utils.toggle_term_with_index(90, "vertical")
     end, { desc = "Open vertical terminal with number" }
 )
 utils.mapfunc(
     'n', '<leader>mo', function()
-        utils.toggle_term_with_number(20, "horizontal")
+        utils.toggle_term_with_index(20, "horizontal")
     end, { desc = "Open horizontal terminal with number" }
 )
 utils.mapfunc(
     'n', '<leader>mt', function()
-        utils.toggle_term_with_number(nil, "tab")
+        utils.toggle_term_with_index(nil, "tab")
     end, { desc = "Open terminal in new tab with number" }
 )
