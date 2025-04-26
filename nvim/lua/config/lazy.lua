a4e215834e9a6484b2cb9fbe37d06ba7b76e0929
DanielTellier <telldanieljames@gmail.com>
2025-04-26 16:23:24 -0700

    Remove markdown plugins.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -105,24 +105,6 @@ local spec = {
     --     },
     -- },
     {
-        "OXY2DEV/markview.nvim",
-        lazy = false, -- Recommended
-        -- ft = "markdown" -- If you decide to lazy-load anyway
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && npm install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
         "folke/flash.nvim",
         event = "VeryLazy",
         ---@type Flash.Config
