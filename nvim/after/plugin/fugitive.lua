5fd6f6f7dcbd5fdf85cceab04ac03c3922f27e2b
DanielTellier <telldanieljames@gmail.com>
2025-07-22 09:47:01 -0700

    Minor change.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,10 @@
 local utils = require('utils')
+local wk = require("which-key")
+
+wk.add({
+    { "<leader>g", group = "fugitive", mode = "n" }
+})
+
 utils.map(
     "n", "<leader>gs", "<cmd>Git<cr>", { desc = "Open git status dashboard" }
 )
