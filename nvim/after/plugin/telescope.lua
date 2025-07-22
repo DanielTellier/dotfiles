21097d1159702183bef0019bf0bfa1725d75dc3e
DanielTellier <telldanieljames@gmail.com>
2025-07-22 09:45:10 -0700

    refactor(telescope): consolidate file browser mappings and fix git files keybinding
    - Merge telescope-file-browser.lua into main telescope.lua config
    - Add which-key group registration for telescope mappings
    - Fix git files keybinding from <leader>gf to <leader>pg for consistency
    - Consolidate all telescope-related mappings in single file for better organization

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,21 @@
 local utils = require('utils')
 local builtin = require('telescope.builtin')
+local wk = require("which-key")
+
+wk.add({
+    { "<leader>p", group = "telescope", mode = "n" }
+})
+
+utils.map(
+    "n", "<leader>po", "<cmd>Telescope file_browser<cr>",
+    { desc = "Run Telescrope file browser mode" }
+)
+utils.map(
+    "n", "<leader>pp", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
+    { desc = "Open file_browser with the path of the current buffer" }
+)
 utils.map('n', '<leader>pf', builtin.find_files, { desc = "Telescope find files" })
-utils.map('n', '<leader>gf', builtin.git_files, { desc = "Telescope git find files" })
+utils.map('n', '<leader>pg', builtin.git_files, { desc = "Telescope git find files" })
 utils.map('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end, { desc = "Telescope grep" })
-
